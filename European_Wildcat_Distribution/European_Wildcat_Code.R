fc260d1af835ab73e0dc13ec9b6c95d9484cdfdf
estermayk <kirbyester@gmail.com>
2025-10-03 13:32:16 +0100

    first commit

---
+++ European_Wildcat_Distribution/European_Wildcat_Code.R
@@ -0,0 +1,6 @@
+install.packages("geodata",dependencies=TRUE,repos="https://cloud.r-project.org")
+install.packages("predicts",dependencies=TRUE,repos="https://cloud.r-project.org")
+install.packages("terra",dependencies=TRUE,repos="https://cloud.r-project.org")
+library(geodata)
+library(predicts)
+library(terra)
